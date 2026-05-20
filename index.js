5c1137f79574adedc3a36408e0af42e07000c65f
arem-anik44 <arem.anik8044@gmail.com>
2026-05-20 19:35:38 +0600

    add global error handler and 404 middleware

--- index.js
+++ index.js
@@ -184,6 +184,15 @@ app.get("/", (req, res) => {
   res.send("SportNest server is running!");
 });
 
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Internal server error" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
